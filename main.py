dc6d65344420f1a43eb9cc8d2591524e754963b8
rvlt1 <rvlt135@gmail.com>
2023-02-28 22:52:46 +0100

    add shemas

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
-from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, Query
 from shemas import Strain
-
 
 app = FastAPI()
 
 
-@app.get('/')
-def main():
+@app.get('/healtcheck')
+def healtcheck():
     return {"status" : "OK"}
 
 
-@app.get('/user/{user_id}/items/{item}')
-def item(user_id: int, item: str = None):
-    return {'pk': user_id, 'query': item}
+@app.get('/api/strain/categories')
+def get_list_categories():
+    return {1: 'Sativa', 2: "Indica", 3: 'Hybrid'}
 
 
-@app.post('/api/strain/')
+@app.post('/api/strain/write')
 def post_strain(item: Strain):
     return item
+
+
+@app.get('/api/strain/')
+def get_strain_name(q: str = Query(None, min_length=2, max_length=1000)):
+    return q
+
+"""
+uvicorn.run(
+    "main.app:app",
+    # host=settings.server_host,
+    # port=settings.server_port,
+    reload=True,
+)"""
--- shemas.py
+++ shemas.py
@@ -1,15 +1,26 @@
 from pydantic import BaseModel
 from typing import List
+from typing import Dict
+
+
+class Categories(BaseModel):
+    list_with_catories = { 1: 'Sativa', 2: "Indica", 3: 'Hybrid'}
+    id: int
+    categories: list = list_with_catories
 
 
 class Effects(BaseModel):
     count: int
     name: str
+    tag: dict = {"name": "Anxious", "type": "Negative"}
+
+
 class Strain(BaseModel):
     id: int
     name: str
     slug_name: str
     description: str
+    reviewsCount: int
     rating: int
     category: str
     countByEffects: List[Effects]
