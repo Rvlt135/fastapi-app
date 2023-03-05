d8c4840e3268404bb500aa7c58c7d6ac0804a5af
rvlt135 <rvlt135@gmail.com>
2023-03-05 10:56:50 +0100

    change

--- models/models.py
+++ models/models.py
@@ -1,6 +1,7 @@
 import datetime
-
-from sqlalchemy import MetaData, Integer, Column, String, TIMESTAMP, ForeignKey, Table, JSON
+from sqlalchemy import MetaData, Column, Integer, String, ForeignKey, Table, JSON, TIMESTAMP
+import sqlalchemy as sa
+from alembic import op
 
 metadata = MetaData()
 
@@ -8,34 +9,37 @@ roles = Table("roles",
               metadata,
               Column("id", Integer, primary_key=True),
               Column("name", String, nullable=False),
-              Column("permissions", JSON)
+              Column("permissions", JSON),
               )
 
 user = Table(
-    "strains",
+    "users",
+    metadata,
     Column("id", Integer, primary_key=True),
     Column("user_mane", String, nullable=False,),
     Column("password", String, nullable=False,),
     Column("email", String, nullable=False,),
-    Column("registred_data", TIMESTAMP, default=datetime.utcnow()),
+    Column("registred_data", TIMESTAMP),
     Column("role_id", Integer, ForeignKey("roles.id")),
 
 )
 
+
+"""
 categories_list = Table(
     "categories",
-    Column("id", Integer, primary_key=True),
-    Column("category", String, nullable=False, )
+    sa.Column("id", Integer, primary_key=True),
+    sa.Column("category", String, nullable=False, )
 
 )
 
 strains = Table(
     "strains",
-    Column("id", Integer, primary_key=True),
-    Column("slug_name", String, nullable=False,),
-    Column("name", String, nullable=False,),
+    sa.Column("id", Integer, primary_key=True),
+    sa.Column("slug_name", String, nullable=False,),
+    sa.Column("name", String, nullable=False,),
     # Column("created_strains", TIMESTAMP, default=datetime.utcnow()),
-    Column("category", String, nullable=False, ),
-    Column("hash_id", String, ForeignKey("categories.category"), nullable=False, ),
-
+    sa.Column("category", String, nullable=False, ),
+    sa.Column("hash_id", String, sa.Text(), ForeignKey("categories.category"), nullable=False, ),
 )
+"""
